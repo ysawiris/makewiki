db90f6c8b09bc5f98353e862db55dcb8f54cd2c8
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-11-07 10:46:31 -0800

    [add] url path for wiki

--- makewiki/urls.py
+++ makewiki/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Wiki App
-    # path('REPLACE_ME_WITH_ROUTE', include(REPLACE_ME_WITH_APP_URLS)),
+    #path('REPLACE_ME_WITH_ROUTE', include(REPLACE_ME_WITH_APP_URLS)),
+    path('wiki/', include('wiki.urls')),
 ]
